02d106e1cfee717353c9a6a70adef4d0f6f513f8
David Eaton <davidzaeaton@outlook.com>
2026-08-30 00:53:13 -0400

    Add comprehensive Money API unit tests

--- Stocktrac.Tests/Unit/Features/Financial/MoneyShould.cs
+++ Stocktrac.Tests/Unit/Features/Financial/MoneyShould.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Shouldly;
 using Stocktrac.Domain.Features.Financial;
 using Stocktrac.Domain.Features.Financial.Extensions;
@@ -7,209 +8,294 @@ namespace Stocktrac.Tests.Unit.Features.Financial;
 public class MoneyShould
 {
     [Fact]
-    public void Treat_All_Decimal_Values_As_Valid_Signed_Amounts()
+    public void Create_WhenGivenAmountAndCurrencyCode_SetsBothProperties()
     {
-        var values = new[] { decimal.MinValue, -1m, 0m, 1m, decimal.MaxValue };
+        var amount = Amount.FromDecimal(12.34m);
+        var currencyCode = CurrencyCode.Create("CAD").Value;
 
-        foreach (var value in values)
-            Amount.FromDecimal(value).Value.ShouldBe(value);
+        var money = Money.Create(amount, currencyCode);
+
+        money.Amount.ShouldBe(amount);
+        money.CurrencyCode.ShouldBe(currencyCode);
     }
 
     [Theory]
     [InlineData("usd", "USD")]
     [InlineData(" EUR ", "EUR")]
-    public void Normalize_Currency_Codes(string input, string expected)
+    [InlineData("XAU", "XAU")]
+    public void Create_WhenGivenValidCurrencyText_ReturnsMoneyWithNormalizedCurrency(
+        string currencyCode,
+        string expectedCurrencyCode)
     {
-        var result = CurrencyCode.Create(input);
+        var result = Money.Create(12.34m, currencyCode);
 
-        result.IsSuccess.ShouldBe(true);
-        result.Value.Value.ShouldBe(expected);
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Amount.ShouldBe(Amount.FromDecimal(12.34m));
+        result.Value.CurrencyCode.Value.ShouldBe(expectedCurrencyCode);
     }
 
     [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    [InlineData("US")]
-    [InlineData("US1")]
-    public void Reject_Invalid_Currency_Codes(string? input)
+    [InlineData(null, CurrencyCode.InvalidMessage)]
+    [InlineData("", CurrencyCode.InvalidMessage)]
+    [InlineData("   ", CurrencyCode.InvalidMessage)]
+    [InlineData("US", CurrencyCode.InvalidMessage)]
+    [InlineData("US1", CurrencyCode.InvalidMessage)]
+    [InlineData("USDD", CurrencyCode.InvalidMessage)]
+    [InlineData("ZZZ", CurrencyCode.UnsupportedMessage)]
+    public void Create_WhenGivenInvalidCurrencyText_ReturnsCurrencyFailure(
+        string? currencyCode,
+        string expectedError)
     {
-        CurrencyCode.Create(input).IsFailure.ShouldBe(true);
+        var result = Money.Create(1m, currencyCode);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(expectedError);
     }
 
     [Theory]
-    [InlineData("XCG")]
-    [InlineData("ZWG")]
-    [InlineData("XAU")]
-    public void Accept_Codes_From_The_Embedded_Iso_4217_List(string input)
+    [InlineData("-79228162514264337593543950335")]
+    [InlineData("-1")]
+    [InlineData("0")]
+    [InlineData("1")]
+    [InlineData("79228162514264337593543950335")]
+    public void Create_WhenGivenAnyDecimalAmount_ReturnsMoney(string amountText)
     {
-        CurrencyCode.Create(input).IsSuccess.ShouldBe(true);
+        var amount = decimal.Parse(amountText, CultureInfo.InvariantCulture);
+
+        var result = Money.Create(amount, "USD");
+
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.Amount.Value.ShouldBe(amount);
     }
 
     [Fact]
-    public void Reject_A_Well_Formed_Code_That_Is_Not_In_The_Iso_4217_List()
+    public void Default_WhenUsed_ReturnsZeroUsd()
     {
-        var result = CurrencyCode.Create("ZZZ");
+        var money = default(Money);
 
-        result.IsFailure.ShouldBe(true);
-        result.Error.ShouldBe(CurrencyCode.UnsupportedMessage);
+        money.Amount.ShouldBe(Amount.FromDecimal(0m));
+        money.CurrencyCode.ShouldBe(CurrencyCode.Usd);
     }
 
     [Fact]
-    public void Create_A_Strongly_Typed_Value()
-    {
-        var amount = Amount.FromDecimal(12.34m);
-        var currencyCode = CurrencyCode.Create("USD").Value;
-
-        var result = Money.Create(amount, currencyCode);
-
-        result.ShouldBeOfType<Money>();
-        result.Amount.ShouldBe(Amount.FromDecimal(12.34m));
-        result.CurrencyCode.Value.ShouldBe("USD");
-        result.ToDisplayString().ShouldBe("12.34 USD");
-    }
-
-    [Fact]
-    public void Use_Value_Equality()
+    public void Equality_WhenAmountAndNormalizedCurrencyAreEqual_ReturnsTrue()
     {
         var first = Money.Create(10m, "USD").Value;
-        var second = Money.Create(10m, "usd").Value;
+        var second = Money.Create(10m, " usd ").Value;
 
+        (first == second).ShouldBeTrue();
         first.ShouldBe(second);
+        first.GetHashCode().ShouldBe(second.GetHashCode());
     }
 
-    [Fact]
-    public void Add_Values_With_The_Same_Currency()
+    [Theory]
+    [InlineData(10, 11, "USD", "USD")]
+    [InlineData(10, 10, "USD", "EUR")]
+    public void Equality_WhenAmountOrCurrencyDiffers_ReturnsFalse(
+        decimal firstAmount,
+        decimal secondAmount,
+        string firstCurrency,
+        string secondCurrency)
     {
-        var left = Money.Create(10.25m, "USD").Value;
-        var right = Money.Create(2.75m, "USD").Value;
+        var first = Money.Create(firstAmount, firstCurrency).Value;
+        var second = Money.Create(secondAmount, secondCurrency).Value;
+
+        (first == second).ShouldBeFalse();
+        (first != second).ShouldBeTrue();
+    }
+
+    [Theory]
+    [InlineData(10.25, 2.75, 13)]
+    [InlineData(-10, 3, -7)]
+    [InlineData(10, -3, 7)]
+    [InlineData(0, 0, 0)]
+    public void Add_WhenCurrenciesMatch_ReturnsSumAndPreservesCurrency(
+        decimal leftAmount,
+        decimal rightAmount,
+        decimal expectedAmount)
+    {
+        var left = Money.Create(leftAmount, "CAD").Value;
+        var right = Money.Create(rightAmount, "CAD").Value;
 
         var result = left.Add(right);
 
-        result.IsSuccess.ShouldBe(true);
-        result.Value.ShouldBe(Money.Create(13m, "USD").Value);
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.ShouldBe(Money.Create(expectedAmount, "CAD").Value);
     }
 
-    [Fact]
-    public void Subtract_Values_With_The_Same_Currency()
+    [Theory]
+    [InlineData(10.25, 2.75, 7.5)]
+    [InlineData(-10, 3, -13)]
+    [InlineData(10, -3, 13)]
+    [InlineData(0, 0, 0)]
+    public void Subtract_WhenCurrenciesMatch_ReturnsDifferenceAndPreservesCurrency(
+        decimal leftAmount,
+        decimal rightAmount,
+        decimal expectedAmount)
     {
-        var left = Money.Create(10.25m, "USD").Value;
-        var right = Money.Create(2.75m, "USD").Value;
+        var left = Money.Create(leftAmount, "GBP").Value;
+        var right = Money.Create(rightAmount, "GBP").Value;
 
-        left.Subtract(right).Value.ShouldBe(Money.Create(7.50m, "USD").Value);
+        var result = left.Subtract(right);
+
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.ShouldBe(Money.Create(expectedAmount, "GBP").Value);
     }
 
     [Fact]
-    public void Reject_Arithmetic_Between_Different_Currencies()
-    {
-        var dollars = Money.Create(10m, "USD").Value;
-        var euros = Money.Create(10m, "EUR").Value;
-
-        var result = dollars.Subtract(euros);
-
-        result.IsFailure.ShouldBe(true);
-        result.Error.ShouldBe(MoneyExtensions.CurrencyMismatchMessage);
-    }
-
-    [Fact]
-    public void Reject_Addition_Between_Different_Currencies()
+    public void Add_WhenCurrenciesDiffer_ReturnsCurrencyMismatchFailure()
     {
         var dollars = Money.Create(10m, "USD").Value;
         var euros = Money.Create(10m, "EUR").Value;
 
         var result = dollars.Add(euros);
 
-        result.IsFailure.ShouldBe(true);
+        result.IsFailure.ShouldBeTrue();
         result.Error.ShouldBe(MoneyExtensions.CurrencyMismatchMessage);
     }
 
     [Fact]
-    public void Multiply_And_Negate_While_Preserving_Currency()
+    public void Subtract_WhenCurrenciesDiffer_ReturnsCurrencyMismatchFailure()
     {
-        var money = Money.Create(4.50m, "GBP").Value;
+        var dollars = Money.Create(10m, "USD").Value;
+        var euros = Money.Create(10m, "EUR").Value;
 
-        money.Multiply(3m).Value.ShouldBe(Money.Create(13.50m, "GBP").Value);
-        money.Negate().Value.ShouldBe(Money.Create(-4.50m, "GBP").Value);
+        var result = dollars.Subtract(euros);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(MoneyExtensions.CurrencyMismatchMessage);
     }
 
     [Fact]
-    public void Return_A_Failure_When_Arithmetic_Overflows()
+    public void Add_WhenResultExceedsDecimalRange_ReturnsOverflowFailure()
     {
-        var maximum = Amount.FromDecimal(decimal.MaxValue);
+        var maximum = Money.Create(decimal.MaxValue, "USD").Value;
 
-        var result = maximum.Add(Amount.FromDecimal(1m));
+        var result = maximum.Add(Money.Create(1m, "USD").Value);
 
-        result.IsFailure.ShouldBe(true);
+        result.IsFailure.ShouldBeTrue();
         result.Error.ShouldBe(Amount.OverflowMessage);
     }
 
     [Fact]
-    public void Return_Failures_For_Overflowing_Amount_Operation()
+    public void Add_WhenResultFallsBelowDecimalRange_ReturnsOverflowFailure()
     {
-        Amount.FromDecimal(decimal.MinValue).Subtract(Amount.FromDecimal(1m)).Error
-            .ShouldBe(Amount.OverflowMessage);
-        Amount.FromDecimal(decimal.MaxValue).Add(Amount.FromDecimal(1m)).Error
-            .ShouldBe(Amount.OverflowMessage);
-        Amount.FromDecimal(decimal.MaxValue).Multiply(2m).Error
-            .ShouldBe(Amount.OverflowMessage);
+        var minimum = Money.Create(decimal.MinValue, "USD").Value;
+
+        var result = minimum.Add(Money.Create(-1m, "USD").Value);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(Amount.OverflowMessage);
     }
 
     [Fact]
-    public void Propagate_Overflow_Failures_Through_Money_Operations()
+    public void Subtract_WhenResultExceedsDecimalRange_ReturnsOverflowFailure()
     {
         var maximum = Money.Create(decimal.MaxValue, "USD").Value;
 
-        maximum.Add(Money.Create(1m, "USD").Value).Error
-            .ShouldBe(Amount.OverflowMessage);
-        maximum.Multiply(2m).Error.ShouldBe(Amount.OverflowMessage);
+        var result = maximum.Subtract(Money.Create(-1m, "USD").Value);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(Amount.OverflowMessage);
+    }
+
+    [Fact]
+    public void Subtract_WhenResultFallsBelowDecimalRange_ReturnsOverflowFailure()
+    {
+        var minimum = Money.Create(decimal.MinValue, "USD").Value;
+
+        var result = minimum.Subtract(Money.Create(1m, "USD").Value);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(Amount.OverflowMessage);
     }
 
     [Theory]
-    [InlineData(null, CurrencyCode.InvalidMessage)]
-    [InlineData("US1", CurrencyCode.InvalidMessage)]
-    [InlineData("ZZZ", CurrencyCode.UnsupportedMessage)]
-    public void Propagate_Currency_Errors_When_Creating_Money(string? code, string expectedError)
+    [InlineData(4.5, 3, 13.5)]
+    [InlineData(4.5, -2, -9)]
+    [InlineData(-4.5, -2, 9)]
+    [InlineData(4.5, 0, 0)]
+    [InlineData(5, 0.5, 2.5)]
+    public void Multiply_WhenResultIsInRange_ReturnsProductAndPreservesCurrency(
+        decimal amount,
+        decimal multiplier,
+        decimal expectedAmount)
     {
-        var result = Money.Create(1m, code);
+        var money = Money.Create(amount, "JPY").Value;
 
-        result.IsFailure.ShouldBe(true);
-        result.Error.ShouldBe(expectedError);
+        var result = money.Multiply(multiplier);
+
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.ShouldBe(Money.Create(expectedAmount, "JPY").Value);
+    }
+
+    [Theory]
+    [InlineData(true, 2)]
+    [InlineData(false, 2)]
+    [InlineData(true, -2)]
+    [InlineData(false, -2)]
+    public void Multiply_WhenProductIsOutsideDecimalRange_ReturnsOverflowFailure(
+        bool useMaximum,
+        decimal multiplier)
+    {
+        var amount = useMaximum ? decimal.MaxValue : decimal.MinValue;
+        var money = Money.Create(amount, "USD").Value;
+
+        var result = money.Multiply(multiplier);
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(Amount.OverflowMessage);
+    }
+
+    [Theory]
+    [InlineData(4.5, -4.5)]
+    [InlineData(-4.5, 4.5)]
+    [InlineData(0, 0)]
+    public void Negate_WhenAmountIsAboveDecimalMinimum_ReturnsOppositeAndPreservesCurrency(
+        decimal amount,
+        decimal expectedAmount)
+    {
+        var money = Money.Create(amount, "AUD").Value;
+
+        var result = money.Negate();
+
+        result.IsSuccess.ShouldBeTrue();
+        result.Value.ShouldBe(Money.Create(expectedAmount, "AUD").Value);
     }
 
     [Fact]
-    public void Format_Amounts_Using_The_Invariant_Culture()
+    public void Negate_WhenAmountIsDecimalMinimum_ReturnsOverflowFailure()
     {
-        var originalCulture = System.Globalization.CultureInfo.CurrentCulture;
+        var minimum = Money.Create(decimal.MinValue, "USD").Value;
+
+        var result = minimum.Negate();
+
+        result.IsFailure.ShouldBeTrue();
+        result.Error.ShouldBe(Amount.OverflowMessage);
+    }
+
+    [Fact]
+    public void ToDisplayString_WhenCalled_UsesInvariantAmountAndCurrencyCode()
+    {
+        var originalCulture = CultureInfo.CurrentCulture;
+
         try
         {
-            System.Globalization.CultureInfo.CurrentCulture =
-                System.Globalization.CultureInfo.GetCultureInfo("fr-FR");
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("fr-FR");
+            var money = Money.Create(1234.50m, "eur").Value;
 
-            Amount.FromDecimal(12.34m).ToString().ShouldBe("12.34");
-            Money.Create(12.34m, "USD").Value.ToDisplayString().ShouldBe("12.34 USD");
+            money.ToDisplayString().ShouldBe("1234.50 EUR");
         }
         finally
         {
-            System.Globalization.CultureInfo.CurrentCulture = originalCulture;
+            CultureInfo.CurrentCulture = originalCulture;
         }
     }
 
     [Fact]
-    public void Create_Usd_Default_Value()
+    public void CurrencyMismatchMessage_WhenRead_DescribesSameCurrencyRequirement()
     {
-        default(CurrencyCode).ShouldBe(CurrencyCode.Usd);
-        default(CurrencyCode).Value.ShouldBe("USD");
-    }
-
-    [Fact]
-    public void Create_Usd_With_Parameterless_Constructor()
-    {
-        new CurrencyCode().ShouldBe(CurrencyCode.Usd);
-    }
-
-    [Fact]
-    public void Create_Explicit_Usd_Default_Value()
-    {
-        CurrencyCode.Create("usd").Value
-            .ShouldBe(default);
+        MoneyExtensions.CurrencyMismatchMessage.ShouldBe(
+            "Money values must have the same currency.");
     }
 }
